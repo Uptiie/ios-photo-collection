58ba6d728daf3d641407b4ff9fa42fefe3a32170
Donovan Langager <donovan.lan16@gmail.com>
2019-07-25 13:07:06 -0700

    Part 2 done

--- Photo App/Photo App/ThemeHelper.swift
+++ Photo App/Photo App/ThemeHelper.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class ThemeHelper {
+    let themePreferenceKey = "themePreferenceKey"
+    var themePreference: String? {
+       let userDefaults = UserDefaults.standard
+        let chosenTheme = userDefaults.string(forKey: themePreferenceKey)
+        return chosenTheme
+    }
+    
+    func setThemePreferenceToDark() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToLight() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Light", forKey: themePreferenceKey)
+    }
+    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+    }
+    
+}
